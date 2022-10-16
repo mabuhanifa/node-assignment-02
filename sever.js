ce3ee9b7cb2d64dbfc16f4b5b5eb1c4c4d8635f6
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-16 16:46:03 +0600

    middleware

--- sever.js
+++ sever.js
@@ -1,1 +1,29 @@
-const express = require('express');
+const express = require("express");
+const errorHandler = require("./middlewares/errorHandler");
+const usersRouter = require("./routes/usersRoute");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/user", usersRouter);
+
+app.get("/", (req, res) => {
+  res.send("Node Application Server Running");
+});
+
+app.all("*", (req, res) => {
+  res.send(`No Route found`);
+});
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  app.close(() => {
+    process.exit(1);
+  });
+});
